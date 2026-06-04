bf64a0fbc2ab4a38365b7ac424f74b7e49957501
Pravalika <pravalikareddy315@gmail.com>
2026-06-04 22:50:32 +0530

    Binary search programs

--- Binary Search/find-x-in-sorted-array.py
+++ Binary Search/find-x-in-sorted-array.py
@@ -1,1 +1,10 @@
+def search(arr, target):
+    n = len(arr)
+    for i in range(n):
+        if arr[i] == target:
+            return i
+    return -1
 
+arr = list(map(int, input().split()))
+target = int(input())
+print(search(arr, target))
