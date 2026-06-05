0ce60757d632ee0acd381dbddc26a806054e93fd
Pravalika <pravalikareddy315@gmail.com>
2026-06-05 13:17:33 +0530

    Binary search programs

--- Binary Search/last-occurance.py
+++ Binary Search/last-occurance.py
@@ -1,2 +1,12 @@
-def last_occurance(arr, target):
-    
+def last_occurrence(arr, target):
+
+    n = len(arr)
+
+    for i in range(n-1, -1, -1):
+        if arr[i] == target:
+            return i
+    return -1
+
+arr = list(map(int, input().split()))
+target = int(input())
+print(last_occurrence(arr, target))
